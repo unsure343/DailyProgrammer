be7344bb9db3dd2e77f876c167869b6b71fcc891
unsure343 <39888641+unsure343@users.noreply.github.com>
2018-07-27 15:34:21 -0700

    Update Mozart.java

--- 343_Mozart/src/Mozart.java
+++ 343_Mozart/src/Mozart.java
@@ -1,17 +1,14 @@
 import java.io.*;
 import java.util.Random;
-import java.util.StringJoiner;
 
-public class Mozart {
+class Mozart {
 
-    private int NewCompositionMeasure;
+    private int NewCompositionCurrentMeasure;
+    private int[] NewCompositionMeasureArray;
     private BufferedWriter FileWriter;
-    private int[] measureArray;
-    private int[][] measures;
 
-    public Mozart () throws IOException {
-        Random rand = new Random();
-        measures =  new int[][] {
+    Mozart() {
+        int[][] OriginalCompositionMeasures =  new int[][] {
                 {96, 32, 69, 40, 148, 104, 152, 119, 98, 3, 54},
                 {22, 6, 95, 17, 74, 157, 60, 84, 142, 87, 130},
                 {141, 128, 158, 113, 163, 27, 171, 114, 42, 165, 10},
@@ -29,53 +26,59 @@ public class Mozart {
                 {109, 116, 145, 52, 1, 23, 89, 72, 149, 173, 44},
                 {14, 83, 79, 170, 93, 151, 172, 111, 8, 78, 131}
         };
+        NewCompositionCurrentMeasure = 1;
+        NewCompositionMeasureArray = new int[16];
+        Random rand = new Random();
+        for (int i = 0; i < 16; i++) {
+            NewCompositionMeasureArray[i] =
+                    OriginalCompositionMeasures[i][rand.nextInt(6) + rand.nextInt(6)];
+            // System.out.println("Chose measure: " + NewCompositionMeasureArray[i]);
+        }
+    }
 
-        NewCompositionMeasure = 1;
+    void CreateComposition() throws Exception {
         File OutputComposition = new File("output.txt");
         FileOutputStream fos = new FileOutputStream(OutputComposition);
         FileWriter = new BufferedWriter(new OutputStreamWriter(fos));
-        measureArray = new int[16];
         for (int i = 0; i < 16; i++) {
-            measureArray[i] = measures[i][rand.nextInt(11)];
-            System.out.println("Chose measure: " + measureArray[i]);
+            GetNextMeasure(".\\src\\original_composition.txt",
+                    NewCompositionMeasureArray[i]);
+            NewCompositionCurrentMeasure++;
+        }
+        FileWriter.close();
+    }
+
+    private void GetNextMeasure(String fileName, int measure) throws IOException {
+        BufferedReader inputFile = new BufferedReader(new FileReader(fileName));
+        String line;
+        while ((line = inputFile.readLine()) != null) {
+            if (IsInMeasure(line, measure)){
+                // System.out.println(line);
+                String nextLine = ConvertLineBeatToNewCompositionBeat(line, measure);
+                // System.out.println(nextLine);
+                AppendNextLineToNewComposition(nextLine);
+            }
         }
     }
-    public Boolean IsInMeasure(String line, int measure) {
+
+    private Boolean IsInMeasure(String line, int measure) {
         String[] noteData = line.split(" ");
         if (((((int) Double.parseDouble(noteData[1])) / 3) + 1) == measure) {
-            System.out.println("int data: " + Double.parseDouble(noteData[1]));
+            // System.out.println("int data: " + Double.parseDouble(noteData[1]));
             return Boolean.TRUE;
         }
         return Boolean.FALSE;
     }
 
-    public Boolean ReadCompositionFile(String fileName, Integer measure) throws IOException {
-        BufferedReader in = new BufferedReader(new FileReader(fileName));
-        String line;
-        while ((line = in.readLine()) != null) {
-
-            if (IsInMeasure(line, measure)){
-                // System.out.println(line);
-                String newLine = ConvertLineToNewCompositionMeasure(line, measure);
-                // System.out.println(newLine);
-                AppendNewLine(newLine);
-            }
-
-        }
-        // only leave this in if you're testing ReadComposition
-        // FileWriter.close();
-        return Boolean.FALSE;
-    }
-
-    private void AppendNewLine(String inputLine) throws IOException {
+    private void AppendNextLineToNewComposition(String inputLine) throws IOException {
         FileWriter.write(inputLine);
         FileWriter.newLine();
     }
 
-    private String ConvertLineToNewCompositionMeasure(String line, int measure) {
-        int converter = (NewCompositionMeasure - measure) * 3;
+    private String ConvertLineBeatToNewCompositionBeat(String line, int measure) {
+        int conversion = (NewCompositionCurrentMeasure - measure) * 3;
         String[] noteData = line.split(" ");
-        Double newBeat = Double.parseDouble(noteData[1]) + converter;
+        Double newBeat = Double.parseDouble(noteData[1]) + conversion;
         if (newBeat == Math.floor(newBeat))
         {
             noteData[1] = Integer.toString(newBeat.intValue());
@@ -85,16 +88,6 @@ public class Mozart {
             noteData[1] = Double.toString(newBeat);
         }
         // System.out.println("original: " + noteData[1] + " new: " + newBeat);
-        String newBeatLine = String.join(" ", noteData);
-        return newBeatLine;
-    }
-
-    public Boolean CreateComposition() throws Exception {
-        for (int i = 0; i < 16; i++) {
-            ReadCompositionFile(".\\src\\original_composition.txt", measureArray[i]);
-            NewCompositionMeasure++;
-        }
-        FileWriter.close();
-        return Boolean.FALSE;
+        return String.join(" ", noteData);
     }
 }
